2571a4d544bf00b2b3a843f7f3e60ae6b69a87d5
xf00889 <hutchiejn@gmail.com>
2025-04-10 06:01:01 +0800

    Add superuser creation to build script

--- build.sh
+++ build.sh
@@ -12,4 +12,10 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput 
+python manage.py collectstatic --noinput
+
+# Create superuser if environment variables are set
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    echo "Creating superuser..."
+    python manage.py createsuperuser --noinput
+fi 
